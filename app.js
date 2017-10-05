a185206e2aae1f4c195c900f3b00b9d5d5736115
Robert Reed <robert.mc.reed@gmail.com>
2017-10-05 15:59:35 -0700

    event listener working for page 1

--- app.js
+++ app.js
@@ -49,16 +49,34 @@ Customer.imgPaths = ( function() {
   return paths;
 })();
 
+Customer.handleAddItem = function( event ) {
+  console.log( 'hi' );
+  event.preventDefault();
+};
 
-( function() {
-  var selectEl = document.getElementsByTagName('select')[ 0 ];
-  Customer.productNames.forEach( function( productName ) {
-    var optionEl = document.createElement( 'option' );
-    optionEl.value = productName;
-    optionEl.textContent = productName;
-    selectEl.appendChild( optionEl );
-  } );
+( function() { //Creates the drop down list for page one
+  if ( document.getElementsByTagName('select')[ 0 ] ) {
+    var selectEl = document.getElementsByTagName('select')[ 0 ];
+    Customer.productNames.forEach( function( productName ) {
+      var optionEl = document.createElement( 'option' );
+      optionEl.value = productName;
+      optionEl.textContent = productName;
+      selectEl.appendChild( optionEl );
+    } );
+  }
 })();
+
+( function() { //Add event listeners to buttons
+  if ( document.getElementsByTagName( 'button' )[ 0 ].name === 'submitcart' ) {
+    var submitCartEl = document.getElementsByTagName( 'button' )[ 0 ];
+    submitCartEl.addEventListener( 'click', Customer.handleAddItem );
+  } else { //event listener for page 2
+    console.log('heyo');
+  }
+})();
+
+
+
 
 
 
