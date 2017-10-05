1c08079b9cf4f9a8088e06ebb5bdea295ca966c7
Robert Reed <robert.mc.reed@gmail.com>
2017-10-05 14:08:14 -0700

    JS started. Constructor working.

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function Customer( name, address ) {
+  this.name = name;
+  this.address = address;
+  this.orders = ( function() {
+    var cart = [];
+    Customer.productNames.forEach( function() {
+      cart.push( 0 );
+    } );
+    return cart;
+  })();
+}
+
+Customer.productNames = [
+  'bag',
+  'banana',
+  'bathroom',
+  'boots',
+  'breakfast',
+  'bubblegum',
+  'chair',
+  'cthulhu',
+  'dog-duck',
+  'dragon',
+  'pen',
+  'pet-sweep',
+  'scissors',
+  'shark',
+  'sweep',
+  'tauntaun',
+  'unicorn',
+  'usb',
+  'water-can',
+  'wine-glass',
+];
+
+Customer.imgPaths = ( function() {
+  var paths = [];
+  Customer.productNames.forEach( function( element, index ) {
+    var ext = '.jpg';
+    if ( element === 'sweep' ) {
+      ext = '.png';
+    } else if ( element === 'usb' ) {
+      ext = '.gif';
+    }
+    paths.push( 'img/' + Customer.productNames[ index ] + ext );
+  });
+  return paths;
+})();
